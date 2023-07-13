79800d3cf8fadfd29ac3fa448e0a505249dc2ea8
zeankun.dev <zeanfender11@gmail.com>
2023-07-13 13:01:00 +0700

    Attempted to add recording feature

--- scripts/RecordingHandler.js
+++ scripts/RecordingHandler.js
@@ -1,1 +1,36 @@
 const { CanvasCapture } = require('canvas-capture')
+const HandlerDate = new Date()
+
+let isRecording = false
+
+CanvasCapture.init(
+    document.getElementById('canvas-renderer'),
+    {
+        verbose: true
+    }
+)
+CanvasCapture.beginVideoRecord({
+    format: CanvasCapture.WEBM,
+    name: `StreamsoftCapture-${HandlerDate.getFullYear()}-${HandlerDate.getMonth()}-${HandlerDate.getDay}-at-${HandlerDate.getHours}-${HandlerDate.getMinutes()}`
+})
+document.getElementById('ui-record-stream').onclick = () => {
+    if (!isRecording) {
+        isRecording = false
+        try {
+            CanvasCapture.stopRecord()
+        } catch (e) {
+            console.error(`Failed to stop recording. Err: ${e}`)
+        }
+    } else {
+        isRecording = true
+        console.log('Set isRecording to true')
+        console.log(`Status of recording in MP4: ${CanvasCapture.browserSupportsMP4()}`)
+        try {
+            CanvasCapture.recordFrame()
+            console.log('Attempted to start recording')
+        } 
+        catch (e) {
+            console.error(`Failed to record. Err: ${e}`)
+        }
+    }
+}
