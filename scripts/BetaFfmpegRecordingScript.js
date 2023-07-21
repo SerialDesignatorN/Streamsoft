8953611fe29ca5ff79b4b4a2b5db6f0222cdd85a
zeankun.dev <zeanfender11@gmail.com>
2023-07-21 16:57:03 +0700

    🛠 NEED FIXING: Attempted to make FFmpeg record

--- scripts/BetaFfmpegRecordingScript.js
+++ scripts/BetaFfmpegRecordingScript.js
@@ -3,8 +3,8 @@ const NodeFS = require('fs')
 const NodePath = require('path')
 const DateTest = new Date()
 const VideoPath = NodePath.join(process.env.USERPROFILE, 'Videos')
-const FfmpegModule = require('ffmpeg.js/ffmpeg-mp4.js')
-const ffmpeg = require('ffmpeg.js')
+const ffmpeg = require('ffmpeg.js/ffmpeg-mp4.js')
+const { stdout } = require('process')
 let isRecording = false
 // `StreamsoftCapture-${DateTest.getFullYear()}-${DateTest.getMonth()}-${DateTest.getDay()}-At-${DateTest.getHours()}-${DateTest.getMinutes()}-${DateTest.getSeconds()}.mp4`
 
@@ -15,28 +15,50 @@ console.log(`Recorder mimetype: ${RecorderMedia.mimeType}`)
 RecorderMedia.ondataavailable = (e) => {
     if (e.data) TemporaryFrames.push(e.data)
 }
+function blobToBuffer(blob) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        resolve(Buffer.from(reader.result));
+      };
+      reader.onerror = reject;
+      reader.readAsArrayBuffer(blob);
+    });
+  }
+  
+  
+  
 
 RecorderMedia.onstop = () => {
     var FileBlob = new Blob(TemporaryFrames, { type: 'video/webm;codecs=h264' })
 
     var BlobReader = new FileReader()
-    BlobReader.onload = (e) => {
+    BlobReader.readAsArrayBuffer(FileBlob)
+    BlobReader.onload = async (e) => {
         var HandlerMP4 = ffmpeg({
-            MEMFS: [{name: 'streamsoft-recording-dump.webm', data: e.target.result}],
-            arguments: ['-i', 'streamsoft-recording-dump.webm', '-vcodec', 'copy', '-qscale', '0', 'streamsoft-mp4-dump.mp4'],
-            stdin: function() {}
-        })
+            MEMFS: [{ name: 'streamsoft-recording-dump.webm', data: e.target.result }],
+            arguments: [
+                '-i', 'streamsoft-recording-dump.webm',
+                '-c:v', 'copy', '-c:a', 'copy', '-strict', 'experimental',
+                '-f', 'mp4',
+                'streamsoft-mp4-dump.mp4'
+            ],
+            stdin: function() {},
+            stdout: function(msg) {
+                console.log(msg)
+            }
+        });
+        
+
         console.log(HandlerMP4.MEMFS)
-        try {
-            var MP4Blob = new Blob([HandlerMP4.MEMFS[0].data], { type: 'video/mp4'})
-        } catch (e) {
-            console.error(`Failed to create blob. Error: ${e}`)
+        var MP4Blob = new Blob([HandlerMP4.MEMFS[0].data], { type: 'video/mp4'})
+        var ConvertedFile = await blobToBuffer(MP4Blob)
+        NodeFS.writeFile(NodePath.join(VideoPath, `StreamsoftCapture-${DateTest.getFullYear()}-${DateTest.getMonth()}-${DateTest.getDay()}-At-${DateTest.getHours()}-${DateTest.getMinutes()}-${DateTest.getSeconds()}.mp4`), ConvertedFile, (err) => {
+            console.error(e)
             CallToast(e, 'error')
-        }
-        NodeFS.writeFile(NodePath.join(VideoPath, `StreamsoftCapture-${DateTest.getFullYear()}-${DateTest.getMonth()}-${DateTest.getDay()}-At-${DateTest.getHours()}-${DateTest.getMinutes()}-${DateTest.getSeconds()}.mp4`), MP4Blob)
+        })
         console.log('Finished saving MP4')
     }
-    BlobReader.readAsArrayBuffer(FileBlob)
 }
 document.getElementById('ui-record-stream').onclick = () => {
     if (isRecording != false) {
